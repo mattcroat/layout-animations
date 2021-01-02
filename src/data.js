d715386668c76e1973e648d6aaa94a08beb67210
mattcroat <mattcroat.dev@gmail.com>
2021-01-02 20:48:50 +0100

    chore: Add cover, tracks information

--- src/data.js
+++ src/data.js
@@ -1,24 +1,77 @@
 export const artists = [
   {
     name: 'Fleetwood Mac',
+    cover:
+      'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-215182-FleetwoodMac_S4_F14-RT.jpg?crop=900:600&width=440',
     albums: [
       {
+        id: 1,
         name: 'Tango in the Night',
         year: '1987',
         length: '44:28',
+        cover:
+          'https://upload.wikimedia.org/wikipedia/en/9/97/Fleetwood_Mac_-_Tango_in_the_Night.png',
         tracks: [
-          'Big Love',
-          'Seven Wonders',
-          'Everywhere',
-          'Caroline',
-          'Tango in the Night',
-          'Mystified',
-          'Little Lies',
-          'Family Man',
-          'Welcome to the Room... Sara',
-          "Isn't It Midnight",
-          'When I See You Again',
-          'You and I, Part II',
+          {
+            trackNumber: 1,
+            title: 'Big Love',
+            length: '3:37',
+          },
+          {
+            trackNumber: 2,
+            title: 'Seven Wonders',
+            length: '3:38',
+          },
+          {
+            trackNumber: 3,
+            title: 'Everywhere',
+            length: '3:48',
+          },
+          {
+            trackNumber: 4,
+            title: 'Caroline',
+            length: '3:50',
+          },
+          {
+            trackNumber: 5,
+            title: 'Tango in the Night',
+            length: '3:56',
+          },
+          {
+            trackNumber: 6,
+            title: 'Mystified',
+            length: '3:08',
+          },
+          {
+            trackNumber: 7,
+            title: 'Little Lies',
+            length: '3:40 ',
+          },
+          {
+            trackNumber: 8,
+            title: 'Family Man',
+            length: '4:08',
+          },
+          {
+            trackNumber: 9,
+            title: 'Welcome to the Room... Sara',
+            length: '3:37',
+          },
+          {
+            trackNumber: 10,
+            title: "Isn't It Midnight",
+            length: '4:06',
+          },
+          {
+            trackNumber: 11,
+            title: 'When I See You Again',
+            length: '3:49',
+          },
+          {
+            trackNumber: 12,
+            title: 'You and I, Part II',
+            length: '2:40',
+          },
         ],
       },
     ],
