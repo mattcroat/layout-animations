7ed788ef1630c3ecdcf47a05e476f7e10aef936c
mattcroat <mattcroat.dev@gmail.com>
2021-01-02 20:46:08 +0100

    feat: Use data to display artist page, and albums

--- src/App.jsx
+++ src/App.jsx
@@ -2,65 +2,91 @@ import React from 'react'
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import { artists } from './data'
+
 export function App() {
+  return <ArtistPage artistName="Fleetwood Mac" />
+}
+
+function ArtistPage({ artistName }) {
+  const artist = artists.find((artist) => artist.name == artistName)
+
   return (
-    <main class="artist-page">
-      <section className="artist-banner">
-        <img
-          class="artist-cover"
-          src="https://images.unsplash.com/photo-1541099413580-823d9a3b59ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-          alt="Cover"
-        />
-
-        <p class="artist-label">Artist</p>
-        <h1 class="artist-name">Fleetwood Mac</h1>
-
-        <div className="actions">
-          <button class="action-play">Play</button>
-          <button class="action-follow">Follow</button>
-          <button class="action-more">&#8230;</button>
-        </div>
-      </section>
-
-      <section className="albums">
-        <h3 class="albums-label">Albums</h3>
-
-        <article className="album">
-          <div className="album-details">
-            <div className="album-cover">
-              <img
-                src="https://images.unsplash.com/photo-1541099413580-823d9a3b59ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-                alt="Cover"
-              />
-            </div>
-
-            <div className="album-info">
-              <p class="info-year">1987</p>
-              <h2 class="info-title">Tango in the Night</h2>
-
-              <div className="actions">
-                <button className="action-heart">🤍</button>
-                <button className="action-more">&#8230;</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="album-tracks">
-            <div className="track-labels">
-              <div className="number">#</div>
-              <div className="heart"></div>
-              <div className="title">Title</div>
-              <div className="duration">🕒</div>
-            </div>
-            <div class="album-track">
-              <div className="number">1</div>
-              <div className="heart">🤍</div>
-              <div className="title">Title</div>
-              <div className="duration">3:00</div>
-            </div>
-          </div>
-        </article>
-      </section>
+    <main className="artist-page">
+      <ArtistInfo artist={artist.name} cover={artist.cover} />
+      <Albums albums={artist.albums} />
     </main>
+  )
+}
+
+function ArtistInfo({ artist, cover }) {
+  return (
+    <section className="artist-banner">
+      <img className="artist-cover" src={cover} alt={artist} />
+
+      <p className="artist-label">Artist</p>
+      <h1 className="artist-name">{artist}</h1>
+
+      <div className="actions">
+        <button className="action-play scale">Play</button>
+        <button className="action-follow scale">Follow</button>
+        <button className="action-more scale">&#8230;</button>
+      </div>
+    </section>
+  )
+}
+
+function Albums({ albums }) {
+  return (
+    <section className="albums">
+      <h3 className="albums-label">Albums</h3>
+      {albums.map((album) => (
+        <Album key={album.id} {...album} />
+      ))}
+    </section>
+  )
+}
+
+function Album({ name, year, cover, tracks }) {
+  return (
+    <article className="album">
+      <div className="album-details">
+        <div className="album-cover">
+          <img src={cover} alt={name} />
+        </div>
+
+        <div className="album-info">
+          <p className="album-year">{year}</p>
+          <h2 className="album-title">
+            <a className="album-link" href="#">
+              {name}
+            </a>
+          </h2>
+
+          <div className="actions">
+            <button className="action-heart scale">🤍</button>
+            <button className="action-more scale">&#8230;</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="album-tracks">
+        <div className="track-labels">
+          <div className="number">#</div>
+          <div className="heart"></div>
+          <div className="title">Title</div>
+          <div className="duration">🕒</div>
+        </div>
+
+        {tracks.map((track) => (
+          <div key={track.trackNumber} className="album-track">
+            <div className="number">{track.trackNumber}</div>
+            <div className="heart">🤍</div>
+            <div className="title">{track.title}</div>
+            <div className="duration">{track.length}</div>
+          </div>
+        ))}
+      </div>
+    </article>
   )
 }
